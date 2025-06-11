88f9ff383d99353e88b8b74d8b70c820558ca165
Max Scheijen <maxscheijen@gmail.com>
2025-06-11 16:04:42 +0200

    failing client test

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,7 +1,6 @@
 import os
 
 import pytest
-from openai import AsyncAzureOpenAI, AsyncOpenAI
 
 from thinky.client import client_selector
 
@@ -13,8 +12,11 @@ def test_client_selector_valid_providers(provider):
     os.environ["OPENAI_API_VERSION"] = "fake-key"
     os.environ["AZURE_OPENAI_API_KEY"] = "fake-key"
     os.environ["AZURE_OPENAI_ENDPOINT"] = "fake-key"
-    client = client_selector(provider)
     if provider == "openai":
-        assert isinstance(client, AsyncOpenAI)
+        with pytest.raises(NotImplementedError):
+            client_selector(provider)
+        # assert isinstance(client, AsyncOpenAI)
     if provider == "azure":
-        assert isinstance(client, AsyncAzureOpenAI)
+        with pytest.raises(NotImplementedError):
+            client_selector(provider)
+    # assert isinstance(client, AsyncAzureOpenAI)
